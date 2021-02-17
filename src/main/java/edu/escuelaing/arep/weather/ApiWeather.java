f49fd9ae61b86b8d3bfce48e4f0f4cc4e29b80f0
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-02-17 14:58:02 -0500

    SparkWeb

--- src/main/java/edu/escuelaing/arep/weather/ApiWeather.java
+++ src/main/java/edu/escuelaing/arep/weather/ApiWeather.java
@@ -1,5 +1,58 @@
 package edu.escuelaing.arep.weather;
 
-public class ApiWeather {
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class ApiWeather {
+	private String url;
+    private String key;
+    private String host;
+
+    public ApiWeather() {
+        url = "http://api.openweathermap.org/data/2.5";
+        key = "74c9871f37742f3c6c61c43a1434f8e2";
+    }
+
+    /**
+     * Obtiene el clima de una ciudad en específico
+     * @param city El nombre de la ciudad a buscar
+     * @return Un string con el clima de la ciudad
+     * @throws ApiConsumerException cuando la ciudad no existe , hay un problema con la conexión o la URL está mal formada
+     */
+    public String getWeatherByCity(String ciudad){
+            URL obj;
+            StringBuffer response = null;
+			try {
+				obj = new URL(url + "/weather?q=" + ciudad+"&appid="+key);
+				HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	            con.setRequestMethod("GET");
+	            int responseCode = con.getResponseCode();
+	            if (responseCode == HttpURLConnection.HTTP_OK) { 
+	                BufferedReader in = new BufferedReader(new InputStreamReader(
+	                        con.getInputStream()));
+	                String inputLine;
+	                response = new StringBuffer();
+	                while ((inputLine = in.readLine()) != null) {
+	                    response.append(inputLine);
+	                }
+	                in.close();
+	            }
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
+            return String.valueOf(response);
+    }
 }
--- src/main/java/edu/escuelaing/arep/weather/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/weather/SparkWebApp.java
@@ -1,13 +1,42 @@
 package edu.escuelaing.arep.weather;
 
+import static spark.Spark.*;
+
+import spark.Request;
+import spark.Response;
+import com.google.gson.Gson;
+
+
 /**
  * Hello world!
  *
  */
+
 public class SparkWebApp
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	private static ApiWeather aw;
+	
+	
+	public static void main(String[] args) {
+		    Gson gson = new Gson();
+	        port(getPort());
+	        aw = new ApiWeather();
+	        get("/clima", (req, res) -> {
+	        	String city = req.queryParams("lugar");
+	            String json = "";
+	            if(city.length()==0){
+	                return "Por favor ingrese un lugar a buscar. Ejemplo : /clima?lugar=Bogota";
+	            }
+	            json = aw.getWeatherByCity(city);
+	            return gson.toJson(json);
+	        });
+	}
+	
+	static int getPort() {
+		if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+	}
+
 }
